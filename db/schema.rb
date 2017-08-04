bb4c29035ab31378410cb72d61b4581b9c52db0d
tulino <tulin.ozen@bil.omu.edu.tr>
2017-08-04 16:46:00 +0300

    Create Product model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170804113020) do
+ActiveRecord::Schema.define(version: 20170804134446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,15 @@ ActiveRecord::Schema.define(version: 20170804113020) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "brand_id"
+    t.integer  "category_id"
+    t.integer  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
@@ -111,4 +120,6 @@ ActiveRecord::Schema.define(version: 20170804113020) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "cities", "countries"
+  add_foreign_key "products", "brands"
+  add_foreign_key "products", "categories"
 end
