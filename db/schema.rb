00f5fbe2e89cace9e0f62d4f086df260af70bb52
tulino <tulin.ozen@bil.omu.edu.tr>
2017-08-04 16:53:53 +0300

    Add description column to  Product table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170804134446) do
+ActiveRecord::Schema.define(version: 20170804135250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 20170804134446) do
     t.integer  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "description"
   end
 
   create_table "users", force: :cascade do |t|
